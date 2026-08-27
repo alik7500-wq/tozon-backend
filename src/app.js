e2a9c401adfa3be8d811faab1778c1ea54c8c97b
Alik <alik7500@gmail.com>
2026-08-28 00:12:13 +0500

    refactor: remove passport OCR feature

--- src/app.js
+++ src/app.js
@@ -28,7 +28,6 @@ import visual3dRouter from './modules/visual3d/visual3d.router.js';
 import tour360Router from './modules/tour360/tour360.router.js';
 import projectMediaRouter from './modules/projectMedia/projectMedia.router.js';
 import searchRouter from './modules/search/search.router.js';
-import documentsRouter from './modules/documents/documents.router.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -85,7 +84,6 @@ app.use('/api', visual3dRouter);
 app.use('/api', tour360Router);
 app.use('/api', projectMediaRouter);
 app.use('/api/search', searchRouter);
-app.use('/api/documents', documentsRouter);
 
 // Global Error Handler
 app.use(errorHandler);
